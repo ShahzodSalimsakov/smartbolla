e3efb33442e24497b203c460e1aaa3eda1a38c9a
shahaa <projects.salim95@gmail.com>
2021-03-02 17:25:32 +0500

    'mainLayout'

---
+++ pages/about/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/Link';
+
+export default function AboutPage() {
+  return (
+      <>
+      <h1>About page</h1>
+      <Link href='/'>
+        <a>Home</a>
+      </Link>
+      </>
+  )
+}
---
+++ pages/invest/index.js
@@ -0,0 +1,5 @@
+function Invest() {
+  return (<></>)
+}
+
+export default Invest
