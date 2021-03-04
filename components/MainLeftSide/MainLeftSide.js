7379c433842cbd73f9bfaf225095a0205e6b6f9a
Davron <yul.davron.93@gmail.com>
2021-03-04 12:35:37 +0400

    left side styles

--- components/MainLeftSide/MainLeftSide.js
+++ components/MainLeftSide/MainLeftSide.js
@@ -3,7 +3,7 @@ import styles from './MainLeftSide.module.css'
 export default function MainLeftSide({children}) {
   return (
       <div className={styles.leftSide}>
-        <div className={styles.rotatedContent}>
+        <div className={`${styles.rotatedContent} flex justify-end`}>
           {children}
         </div>
       </div>
