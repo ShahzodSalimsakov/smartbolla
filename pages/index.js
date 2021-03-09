39250ff7a30ae0d8796342b10660088857a396cb
shahaa <projects.salim95@gmail.com>
2021-03-09 19:22:21 +0500

    social edit

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,7 @@
-import Link from 'next/link';
+import Link from "next/link";
 import Head from "next/head";
-import {MainLayout} from "../components/MainLayout";
+import { MainLayout } from "../components/MainLayout";
 
 export default function Home() {
-  return (
-    <MainLayout title={'Smartbolla'}>
-
-    </MainLayout>
-  )
+  return <MainLayout title={"Smartbolla"} />;
 }
