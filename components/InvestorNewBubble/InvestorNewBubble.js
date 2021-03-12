6b80fe784395100d97ffa6422da64391b15d301d
Davron <yul.davron.93@gmail.com>
2021-03-12 20:38:25 +0500

    investor list

---
+++ components/InvestorNewBubble/InvestorNewBubble.js
@@ -0,0 +1,92 @@
+import { useEffect } from "react";
+import BubbleUI from "react-bubble-ui";
+import { useInView } from "react-intersection-observer";
+import "react-bubble-ui/dist/index.css";
+import { motion } from "framer-motion";
+import { useRouter } from "next/router";
+
+export default function InvestorNewBubble({ investors }) {
+  const { ref, inView } = useInView();
+  const router = useRouter();
+  const options = {
+    size: 180,
+    minSize: 20,
+    gutter: 8,
+    provideProps: true,
+    numCols: 6,
+    fringeWidth: 160,
+    yRadius: 130,
+    xRadius: 220,
+    cornerRadius: 50,
+    showGuides: false,
+    compact: true,
+    gravitation: 5,
+  };
+  const children = investors.map((data, i) => {
+    return (
+      <div
+        key={data.ID}
+        className="child-block cursor-pointer"
+        onClick={() => router.push("/investor/" + data.ID)}
+        layoutId="investorPhoto"
+      >
+        <div
+          className="child cursor-pointer"
+          style={{ backgroundImage: `url(${data.PREVIEW_PICTURE})` }}
+        ></div>
+      </div>
+    );
+  });
+  return (
+    <>
+      <div ref={ref}>
+        <motion.div
+          initial={{
+            y: 100,
+            opacity: 0,
+          }}
+          animate={{
+            y: 0,
+            opacity: 1,
+          }}
+        >
+          {inView && (
+            <BubbleUI options={options} className="myBubbleUI">
+              {children}
+            </BubbleUI>
+          )}
+        </motion.div>
+      </div>
+      <style jsx global>
+        {`
+          .myBubbleUI {
+            width: 100%;
+            max-width: 100vw;
+            height: 80vh;
+            border-radius: 50px;
+          }
+          .child-block {
+            width: 100%;
+            height: 100%;
+            border-radius: 50%;
+            background-color: #ccc;
+            background-size: cover;
+            transition: all 0.5s ease;
+          }
+
+          .child {
+            width: 100%;
+            height: 100%;
+            border-radius: 50%;
+            background-color: #ccc;
+            background-size: cover;
+            transition: all 0.5s ease;
+          }
+          .child:hover {
+            transform: scale(1.2);
+          }
+        `}
+      </style>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,9 @@ import { useDispatch } from "react-redux";
 import { changeMainBackground } from "../store/actions/mainConfigActions";
 import { motion } from "framer-motion";
 import FullPageSectionTitle from "../components/FullPageSectionTitle/FullPageSectionTitle";
+import InvestorNewBubble from "../components/InvestorNewBubble/InvestorNewBubble";
+import dynamic from "next/dynamic";
+import TrackVisibility from "react-on-screen";
 
 const pluginWrapper = () => {
   require("../public/js/scrolloverflow.min");
@@ -17,63 +20,68 @@ function Home({ investors }) {
   const dispatch = useDispatch();
 
   return (
-    <MainLayout title={"Smartbolla"}>
-      <ReactFullpage
-        //fullpage options
-        licenseKey={""}
-        scrollingSpeed={1000} /* Options here */
-        scrollOverflow={true}
-        navigation={true}
-        navigationPosition={"left"}
-        sectionsColor={["#282c34", "#ff5f45", "#0798ec"]}
-        onLeave={(origin, destination, direction) => {
-          // useEffect(() => {
-          // dispatch(changeMainBackground("red"));
-          // }, []);
-        }}
-        render={({ state, fullpageApi }) => {
-          return (
-            <ReactFullpage.Wrapper className="">
-              <div className="section pl-24">
-                <div className="flex">
-                  <div className="flex h-100 items-center">
-                    <motion.h1
-                      initial={{ scale: [14, 1] }}
-                      animate={{ scale: [1, 1] }}
-                      transition={{ duration: 0.8, ease: "easeIn", delay: 2 }}
-                    >
-                      SmartBolla
-                    </motion.h1>
+    <>
+      <MainLayout title={"Smartbolla"}>
+        <ReactFullpage
+          //fullpage options
+          licenseKey={""}
+          scrollingSpeed={1000} /* Options here */
+          navigation={true}
+          navigationPosition={"left"}
+          sectionsColor={["#282c34", "#ff5f45", "#0798ec"]}
+          onLeave={(origin, destination, direction) => {
+            if (origin.index == 1 && direction == "up") {
+              return false;
+            }
+            // useEffect(() => {
+            // dispatch(changeMainBackground("red"));
+            // }, []);
+          }}
+          render={({ state, fullpageApi }) => {
+            return (
+              <ReactFullpage.Wrapper className="">
+                <div className="section pl-24">
+                  <div className="flex">
+                    <div className="flex h-100 items-center">
+                      <motion.h1
+                        initial={{ scale: [14, 1] }}
+                        animate={{ scale: [1, 1] }}
+                        transition={{ duration: 0.8, ease: "easeIn", delay: 2 }}
+                      >
+                        SmartBolla
+                      </motion.h1>
+                    </div>
+                    <div></div>
                   </div>
-                  <div></div>
                 </div>
-              </div>
-              <div className="section pl-24 pt-20">
-                <FullPageSectionTitle title="Investors" />
-                {investors && <InvestorsBubble investors={investors} />}
-              </div>
-            </ReactFullpage.Wrapper>
-          );
-        }}
-      />
-      <style jsx global>
-        {`
-          #fp-nav ul li a span,
-          .fp-slidesNav ul li a span {
-            background: transparent !important;
-            border: 3px solid #fff !important;
-            width: 12px !important;
-            height: 12px !important;
-            margin: -6px 0 0 -6px !important;
-          }
-          #fp-nav ul li a.active span,
-          .fp-slidesNav ul li a.active span {
-            background: #fff !important;
-            border: 3px solid #fff !important;
-          }
-        `}
-      </style>
-    </MainLayout>
+                <div className="section pl-24 pt-20">
+                  <FullPageSectionTitle title="Investors" />
+                  {/*investors && <InvestorsBubble investors={investors} />*/}
+                  {investors && <InvestorNewBubble investors={investors} />}
+                </div>
+              </ReactFullpage.Wrapper>
+            );
+          }}
+        />
+        <style jsx global>
+          {`
+            #fp-nav ul li a span,
+            .fp-slidesNav ul li a span {
+              background: transparent !important;
+              border: 3px solid #fff !important;
+              width: 12px !important;
+              height: 12px !important;
+              margin: -6px 0 0 -6px !important;
+            }
+            #fp-nav ul li a.active span,
+            .fp-slidesNav ul li a.active span {
+              background: #fff !important;
+              border: 3px solid #fff !important;
+            }
+          `}
+        </style>
+      </MainLayout>
+    </>
   );
 }
 
