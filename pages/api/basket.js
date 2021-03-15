d6bd6f25a61dfaf503ec172b64032fd211fbfeb7
Davron <yul.davron.93@gmail.com>
2021-03-15 20:36:06 +0500

    added product

--- pages/api/basket.js
+++ pages/api/basket.js
@@ -1,19 +1,26 @@
 export default async function handler(req, res) {
-  if (req.method === "POST") {
-    try {
-      const resCounter = await fetch("https://smartbolla.com/api/", {
-        method: "POST",
-        body: req.body,
-        headers: {
-          ApiToken: "e7r8uGk5KcwrzT6CanBqRbPVag8ILXFC",
+  //   if (req.method === "POST") {
+  //   console.log(req.body);
+  console.log(req.query);
+  try {
+    const resCounter = await fetch("https://smartbolla.com/api/", {
+      method: "POST",
+      body: JSON.stringify({
+        method: req.query.method,
+        data: {
+          id: req.query.id,
         },
-      });
-    } catch (e) {
-      res.json({
-        result: false,
-      });
-    }
+      }),
+      headers: {
+        ApiToken: "e7r8uGk5KcwrzT6CanBqRbPVag8ILXFC",
+      },
+    });
+  } catch (e) {
+    res.json({
+      result: false,
+    });
   }
+  //   }
   res.json({
     result: true,
   });
