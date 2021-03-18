2940ad592ee62c8b9d0cda35c1404e0bf10903b1
shahaa <projects.salim95@gmail.com>
2021-03-18 11:06:24 +0500

    footer changed desktop

--- components/Footer/Footer.desktop.js
+++ components/Footer/Footer.desktop.js
@@ -7,13 +7,13 @@ export default function Footer() {
     <div
       className={`${styles.footerbody} bottom-0 fixed text-white right-0 uppercase font-extralight`}
     >
-      <div className="border-t mb-10">
-        <p className="float-right mr-16 text-gray-400">
+      <div className="border-t flex">
+        <p className="m-0 ml-auto mt-2 position-relative pr-10 text-gray-400">
           &copy; {year} Smartbolla. all rights reserved
         </p>
       </div>
-      <div className="border-t">
-        <p className="float-right mr-16 text-gray-400">
+      <div className="border-t flex">
+        <p className="m-0 mb-2 ml-auto pr-10 text-gray-400">
           We would like to hear from you
           <a
             href="mailto:info@smartbolla.com"
